07b7fa3c66c27650cd80a33d596975b2d00f8db3
Andre <andrevitoreur@gmail.com>
2023-03-19 17:37:43 +0000

    configure github actions

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 app.get('/names', (req, res) => res.send('Andre'))
 app.listen(port, () => console.log(`App is running on port ${port}!`))
